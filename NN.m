22d8ad34eb3b04959691597e4930fa4db5107b98
Kha <44247554+Nguyen-Kha@users.noreply.github.com>
2022-02-24 13:59:36 -0500

    NN and KNN (#5)
    * NN classifier untested
    
    * Nearest Neighbours
    
    * the real NN
    
    * starting KNN
    
    * NN and KNN
    
    * support functions for KNN

---
+++ NN.m
@@ -0,0 +1,60 @@
+function [ class ] = NN(point, class_data_1, class_data_2, class_data_3)
+    class = 0
+    length_var_1 = length(class_data_1);
+    length_var_2 = length(class_data_2);
+    
+    distances_1 = zeros(length_var_1, 1);
+    distances_2 = zeros(length_var_2, 1);
+    
+    % For class 1
+    for i=1 : length_var_1
+        temp_dist = point - [class_data_1(i,1); class_data_1(i,2)];
+        distances_1(i) = sqrt(temp_dist(1)^2 + temp_dist(2)^2);
+    end
+    
+    % For class 2
+    for i=1 : length_var_2
+        temp_dist = point - [class_data_2(i,1); class_data_2(i,2)];
+        distances_2(i) = sqrt(temp_dist(1)^2 + temp_dist(2)^2);
+    end
+    
+    % Case 2
+    % if class 3 exists
+    if (exist('class_data_3', 'var'))
+        length_var_3 = length(class_data_3);
+        distances_3 = zeros(length_var_3, 1);
+    
+        for i=1 : length_var_3
+            temp_dist = point - [class_data_3(i,1); class_data_3(i,2)];
+            distances_3(i) = sqrt(temp_dist(1)^2 + temp_dist(2)^2);
+        end
+    
+        min_distances = [min(distances_1), min(distances_2), min(distances_3)];
+        if((min(min_distances) == min(distances_1)) && (min(distances_1) == min(distances_2)))
+            class = 0;
+        elseif((min(min_distances) == min(distances_1)) && (min(distances_1) == min(distances_3)))
+            class = 0;
+        elseif((min(min_distances) == min(distances_2)) && (min(distances_2) == min(distances_3)))
+            class = 0;
+        elseif((min(distances_1) == min(distances_2)) && (min(distances_2) == min(distances_3)))
+            class = 0;
+        elseif min(min_distances) == min(distances_1)
+            class = 1;
+        elseif min(min_distances) == min(distances_2)
+            class = 2;
+        elseif min(min_distances) == min(distances_3)
+            class = 3;
+        end
+    
+    else
+        min_distances = [min(distances_1), min(distances_2)];
+        if min(distances_1) == min(distances_2)
+            class = 0;
+        elseif min(min_distances) == min(distances_1)
+            class = 1;
+        elseif min(min_distances) == min(distances_2)
+            class = 2;
+        end
+    end
+
+end
--- lab1.m
+++ lab1.m
@@ -232,3 +232,97 @@ xlabel('x');
 ylabel('y');
 title('MAP Classification Case 2');
 legend('Decision Boundaries','Class C','Class D', 'Class E');
+
+[XNN,YNN] = meshgrid(x1, x2);
+AB_NN = zeros(size(XNN,1), size(YNN,1));
+CDE_NN = zeros(size(XNN,1), size(YNN,1));
+
+for i=1:length(x1)
+    for j=1:length(x2)
+        point = [x1(i); x2(j)];
+        AB_NN(i,j) = NN(point, class_a_data, class_b_data);
+        CDE_NN(i,j) = NN(point, class_c_data, class_d_data, class_e_data);
+    end
+end
+
+figure
+imagesc(x1,x2,AB_NN')
+hold on
+contour(XNN, YNN, AB_NN');
+hold on
+scatter(class_a_data(:,1), class_a_data(:,2), 'filled')
+hold on
+scatter(class_b_data(:,1), class_b_data(:,2), 'filled')
+plot_ellipse(mean_a(1), mean_a(2), 0, sqrt(covar_a(1,1)), sqrt(covar_a(2,2)), 'black')
+plot_ellipse(mean_b(1), mean_b(2), 0, sqrt(covar_b(1,1)), sqrt(covar_b(2,2)), 'black')
+xlabel('x');
+ylabel('y');
+title('NN Classification Case 1');
+legend('Decision Boundaries', 'Class A','Class B');
+set(gca, 'YDir', 'normal')
+
+figure
+imagesc(x1,x2,CDE_NN')
+hold on
+contour(XNN, YNN, CDE_NN');
+hold on
+scatter(class_c_data(:,1), class_c_data(:,2), 'filled')
+hold on
+scatter(class_d_data(:,1), class_d_data(:,2), 'filled')
+scatter(class_e_data(:,1), class_e_data(:,2), 'filled')
+
+plot_ellipse(mean_c(1), mean_c(2), atan(V_c(2,2) / V_c(1,2)), sqrt(covar_c(2,2)), sqrt(covar_c(1,1)), 'black')
+plot_ellipse(mean_d(1), mean_d(2), 0, sqrt(covar_d(1,1)), sqrt(covar_d(2,2)), 'black')
+plot_ellipse(mean_e(1), mean_e(2), atan(V_e(2,2) / V_e(1,2)), sqrt(covar_e(2,2)), sqrt(covar_e(1,1)), 'black')
+xlabel('x');
+ylabel('y');
+title('NN Classification Case 2');
+legend('Decision Boundaries', 'Class C','Class D', 'Class E');
+set(gca, 'YDir', 'normal')
+
+[XKNN,YKNN] = meshgrid(x1, x2);
+AB_KNN = zeros(size(XKNN,1), size(YKNN,1));
+CDE_KNN = zeros(size(XKNN,1), size(YKNN,1));
+
+k = 5;
+for i=1:length(x1)
+    for j=1:length(x2)
+        point = [x1(i); x2(j)];
+        AB_KNN(i,j) = kNN(k, point, class_a_data, class_b_data);
+        CDE_KNN(i,j) = kNN(k, point, class_c_data, class_d_data, class_e_data);
+    end
+end
+
+figure
+imagesc(x1,x2,AB_KNN')
+hold on
+contour(XKNN, YKNN, AB_KNN');
+hold on
+scatter(class_a_data(:,1), class_a_data(:,2), 'filled')
+hold on
+scatter(class_b_data(:,1), class_b_data(:,2), 'filled')
+plot_ellipse(mean_a(1), mean_a(2), 0, sqrt(covar_a(1,1)), sqrt(covar_a(2,2)), 'black')
+plot_ellipse(mean_b(1), mean_b(2), 0, sqrt(covar_b(1,1)), sqrt(covar_b(2,2)), 'black')
+xlabel('x');
+ylabel('y');
+title('kNN Classification Case 1');
+legend('Decision Boundaries', 'Class A','Class B');
+set(gca, 'YDir', 'normal')
+
+figure
+imagesc(x1,x2,CDE_KNN')
+hold on
+contour(XKNN, YKNN, CDE_KNN');
+hold on
+scatter(class_c_data(:,1), class_c_data(:,2), 'filled')
+hold on
+scatter(class_d_data(:,1), class_d_data(:,2), 'filled')
+scatter(class_e_data(:,1), class_e_data(:,2), 'filled')
+plot_ellipse(mean_c(1), mean_c(2), atan(V_c(2,2) / V_c(1,2)), sqrt(covar_c(2,2)), sqrt(covar_c(1,1)), 'black')
+plot_ellipse(mean_d(1), mean_d(2), 0, sqrt(covar_d(1,1)), sqrt(covar_d(2,2)), 'black')
+plot_ellipse(mean_e(1), mean_e(2), atan(V_e(2,2) / V_e(1,2)), sqrt(covar_e(2,2)), sqrt(covar_e(1,1)), 'black')
+xlabel('x');
+ylabel('y');
+title('kNN Classification Case 2');
+legend('Decision Boundaries', 'Class C','Class D', 'Class E');
+set(gca, 'YDir', 'normal')
