d69841ffd512b91e8d69fab3447c2a35a27dc88a
Kha <44247554+Nguyen-Kha@users.noreply.github.com>
2022-03-31 12:13:20 -0400

    Non Parametric 2D  (#14)
    * plots for the original plots
    
    * Question 2 with equiprobability contours
    
    * Add GED/MICD classifier (#2)
    
    * Add MAP Classifier (#3)
    
    * med (#4)
    
    * better label for MED plots
    
    * some small fixes
    
    * NN and KNN (#5)
    
    * NN classifier untested
    
    * Nearest Neighbours
    
    * the real NN
    
    * starting KNN
    
    * NN and KNN
    
    * support functions for KNN
    
    * Add experimental error function
    
    * Plot MAP, MICD, MED together
    
    * Add GED error (#6)
    
    * MAP Confusion matrix and experimental error (#7)
    
    * MED Error Analysis (#8)
    
    * med error analysis
    
    * better description
    
    * remove new lines
    
    * reformat
    
    * whoops
    
    * NN and KNN confusion matrix and P(e) (#9)
    
    * NN error functions
    
    * NN Confusion matrix and P(e)
    
    * KNN confusion matrix and P(e)
    
    * minor variable name change
    
    * Update nn_point_to_map.m
    
    * changing names of plots
    
    * Add plots for NN and KNN on same graph
    
    * non parametric 2D fingers crossed github doesnt die
    
    * i dont know what went wrong but i hope this fixes it
    
    Co-authored-by: Lynn Kim <37675216+lynn-kim@users.noreply.github.com>
    Co-authored-by: Ahmad Chaudhry <ahmadtc2000@gmail.com>
    Co-authored-by: Daniel Kim <dannykim200@gmail.com>
    Co-authored-by: lynn-kim <lynnkim2000@gmail.com>

--- lab2/lab2.m
+++ lab2/lab2.m
@@ -56,4 +56,42 @@ legend('ML Decision Boundary', 'Class A Data', 'Class B Data', 'Class C Data');
 xlabel('x');
 ylabel('y');
 
+% NON-PARAMETRIC ESTIMATION
+h = 1;
+x1 = x_min-1:h:x_max+1;
+y1 = y_min-1:h:y_max+1;
+[X1, Y1] = meshgrid(x1, y1);
+abc_non_parametric = zeros(size(X1,1), size(Y1,2));
 
+res = [h x_min y_min x_max y_max]; % From the parzen file
+
+% Setting up for Parzen
+%%% win = window
+%%% The window function in lab 2 is a Gaussian Window function, with
+%%% variance of 400
+window = gaussian_window(400);
+
+[p_al, x_al, y_al] = parzen(al, res, window);
+[p_bl, x_bl, y_bl] = parzen(bl, res, window);
+[p_cl, x_cl, y_cl] = parzen(cl, res, window);
+
+for i = 1:size(X1, 1) -1
+    for j = 1:size(Y1, 2)-1
+        val1 = p_al(i,j);
+        val2 = p_bl(i,j);
+        val3 = p_cl(i,j);
+        [max_val, class] = max([val1 val2 val3]);
+        abc_non_parametric(i,j) = class;
+    end
+end
+
+figure
+% Plot ML contour for classes a,b,c
+contour(X1,Y1,abc_non_parametric,'green');
+hold on
+scatter(al(:,1), al(:,2), color='red')
+scatter(bl(:,1), bl(:,2), color='blue')
+scatter(cl(:,1), cl(:,2), color='yellow')
+legend('ML Decision Boundary', 'Class A Data', 'Class B Data', 'Class C Data');
+xlabel('x');
+ylabel('y');
