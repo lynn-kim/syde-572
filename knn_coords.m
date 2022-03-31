c06d21571e8245628b30402faf6317e441029238
Nguyen-Kha <khnguyen26@gmail.com>
2022-03-31 12:12:52 -0400

    i dont know what went wrong but i hope this fixes it

--- knn_coords.m
+++
@@ -1,8 +0,0 @@
-function [coords, distances] = knn_coords(k, data, distances, coords)
-    for i=1: k
-        [minimum, index] = min(distances);
-        coords(i, 1) = data(index, 1);
-        coords(i, 2) = data(index, 2);
-        distances(index) = inf;
-    end
-end
